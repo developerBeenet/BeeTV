57038b4612a3f442f43e147a8c441d53d08656f5
LuisT <lusart87@gmail.com>
2020-11-25 11:09:43 -0600

    commit

--- app/src/main/java/com/example/theo_androidtv/ui/PlayerActivity.java
+++ app/src/main/java/com/example/theo_androidtv/ui/PlayerActivity.java
@@ -94,8 +94,7 @@ public class PlayerActivity extends AppCompatActivity {
 
     THEOplayerView theoplayerView;
     Player player;
-
-
+    
     public String stream = " ";
     String auth, id_category = "0";
 
@@ -134,7 +133,6 @@ public class PlayerActivity extends AppCompatActivity {
         configureTheoPlayer(stream);
 
     }
-
 
     private void configureTheoPlayer(String url) {
         // Creating a TypedSource builder that defines the location of a single stream source
